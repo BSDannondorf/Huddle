7097a8da5b492c21081ed060834843b51b5d50c4
BSDannondorf <dannondorf@gmail.com>
2024-12-03 11:37:59 -0500

    1.0

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Huddle/', // Replace 'Huddle' with your repo name if deploying to GitHub Pages
-  build: {
-    rollupOptions: {
-      input: './src/main.jsx', // Explicitly specify your entry point
-    },
-  },
+  base: '/Huddle/', // If deploying to GitHub Pages, adjust as necessary
 });
